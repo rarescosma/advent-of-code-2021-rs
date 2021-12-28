71e3967acce8c4db6b57af62784fc0b1a110474b
Rareș Cosma <rares@getbetter.ro>
2021-12-28 23:02:48 +0200

    demote main

--- days/src/bin/day01.rs
+++ days/src/bin/day01.rs
@@ -1,3 +1,15 @@
+#[inline]
+fn count_increases(xs: &[i64]) -> usize {
+    xs.windows(2).filter(|&slice| slice[1] > slice[0]).count()
+}
+
+fn read_input() -> Vec<i64> {
+    include_str!("../../inputs/day01.txt")
+        .lines()
+        .flat_map(|s| s.parse::<i64>())
+        .collect::<Vec<_>>()
+}
+
 aoc2021::main! {
     let input = read_input();
 
@@ -10,16 +22,4 @@ aoc2021::main! {
     );
 
     (p1, p2)
-}
-
-#[inline]
-fn count_increases(xs: &[i64]) -> usize {
-    xs.windows(2).filter(|&slice| slice[1] > slice[0]).count()
-}
-
-fn read_input() -> Vec<i64> {
-    include_str!("../../inputs/day01.txt")
-        .lines()
-        .flat_map(|s| s.parse::<i64>())
-        .collect::<Vec<_>>()
 }
