a072bc24b7dad7a0e79c23b4bb2d255072ce6b13
Rareș Cosma <rares@getbetter.ro>
2021-12-30 11:18:00 +0200

    dijsktra: don't require Clone on GameState

--- crates/aoc_dijsktra/src/lib.rs
+++ crates/aoc_dijsktra/src/lib.rs
@@ -1,57 +1,68 @@
 use std::cmp::Reverse;
 use std::collections::BinaryHeap;
-use std::hash::Hash;
+use std::hash::{BuildHasher, Hash, Hasher};
 
+use ahash::RandomState;
 use hashbrown::hash_map::Entry;
 use hashbrown::HashMap;
+use lazy_static::lazy_static;
 
-pub trait GameState<V, C>
+lazy_static! {
+    static ref BUILDER: RandomState = RandomState::new();
+}
+
+pub trait Transform<G> {
+    fn cost(&self) -> usize;
+    fn transform(&self, game_state: &G) -> G;
+}
+
+pub trait GameState<V, C>: Ord + Hash + Sized
 where
     V: IntoIterator,
     <V as IntoIterator>::Item: Transform<Self>,
 {
     fn accept(&self) -> bool;
     fn steps(&self, context: &C) -> V;
-}
 
-pub trait Transform<S: ?Sized> {
-    fn cost(&self) -> usize;
-    fn transform(&self, game_state: &S) -> S;
-}
+    /// compute the shortest path through a graph of costs and states
+    fn dijsktra(self, context: &C) -> Option<usize> {
+        let mut visited = HashMap::new();
 
-/// compute the shortest path through a graph of costs and states
-pub fn dijsktra<V, S, C>(initial_state: S, context: &C) -> Option<usize>
-where
-    V: IntoIterator,
-    <V as IntoIterator>::Item: Transform<S>,
-    S: GameState<V, C> + Ord + Clone + Hash,
-{
-    let mut pq = BinaryHeap::new();
-    let mut visited = HashMap::new();
-    pq.push((Reverse(0), initial_state));
-    while let Some((Reverse(cost), state)) = pq.pop() {
-        if state.accept() {
-            return Some(cost);
-        }
-        for step in state.steps(context) {
-            let cost = cost + step.cost();
-            let new_state = step.transform(&state);
+        let mut pq = BinaryHeap::new();
+        pq.push((Reverse(0), self));
 
-            match visited.entry(new_state.clone()) {
-                Entry::Occupied(mut entry) => {
-                    // If this state is already known we only keep it if the
-                    // cost is less.
-                    if cost < *entry.get() {
+        while let Some((Reverse(cost), state)) = pq.pop() {
+            if state.accept() {
+                return Some(cost);
+            }
+            for step in state.steps(context) {
+                let cost = cost + step.cost();
+                let new_state = step.transform(&state);
+                let new_hash = manual_hash(&new_state);
+
+                match visited.entry(new_hash) {
+                    // can we get to this (alread seen) state with a reduced cost?
+                    Entry::Occupied(mut entry) if cost < *entry.get() => {
                         entry.insert(cost);
                         pq.push((Reverse(cost), new_state));
                     }
-                }
-                Entry::Vacant(entry) => {
-                    entry.insert(cost);
-                    pq.push((Reverse(cost), new_state));
+                    Entry::Vacant(entry) => {
+                        entry.insert(cost);
+                        pq.push((Reverse(cost), new_state));
+                    }
+                    _ => (),
                 }
             }
         }
+        None
     }
-    None
+}
+
+fn manual_hash<H>(what: H) -> u64
+where
+    H: Hash,
+{
+    let mut hasher = BUILDER.build_hasher();
+    what.hash(&mut hasher);
+    hasher.finish()
 }
