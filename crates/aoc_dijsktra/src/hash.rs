c74e22c144e1e359ecdef66328b9b356d43875e5
Rareș Cosma <rares@getbetter.ro>
2022-01-02 18:08:31 +0200

    add wrapping struct for the non-owning keymap

--- crates/aoc_dijsktra/src/hash.rs
+++ crates/aoc_dijsktra/src/hash.rs
@@ -1,7 +1,13 @@
+/// We want to keep track of the costs of visited GameStates, but
+/// inserting into `HashMap`s requires owned values, so we use a `KeyMap`
+/// instead which uses `manually_hash`, then inserts the resulting hash
+/// in a regular hash table set up to use a `MirrorHasher` which simply
+/// mirrors back the `u64` produced by `manually_hash`.
 use std::hash::{BuildHasher, BuildHasherDefault};
 use std::hash::{Hash, Hasher};
 
 use ahash::RandomState;
+use hashbrown::hash_map::Entry;
 use hashbrown::HashMap;
 use lazy_static::lazy_static;
 
@@ -9,17 +15,28 @@ lazy_static! {
     static ref HASHER_BUILDER: RandomState = RandomState::new();
 }
 
-/// We want to keep track of the costs of visited GameStates, but
-/// inserting into `HashMap`s requires owned values, so we `manually_hash`
-/// instead to avoid the extra cloning (and the `Clone` bound).
-pub(crate) fn manually_hash<H: Hash>(state: &H) -> u64 {
+pub(crate) struct KeyMap {
+    hashmap: HashMap<u64, usize, MirrorHashBuilder>,
+}
+
+impl KeyMap {
+    pub fn entry<H: Hash>(&mut self, k: &H) -> Entry<u64, usize, MirrorHashBuilder> {
+        self.hashmap.entry(manually_hash(k))
+    }
+}
+
+impl Default for KeyMap {
+    fn default() -> Self {
+        Self {
+            hashmap: HashMap::with_capacity_and_hasher(1024, MirrorHashBuilder::default()),
+        }
+    }
+}
+
+fn manually_hash<H: Hash>(state: &H) -> u64 {
     let mut hasher = HASHER_BUILDER.build_hasher();
     state.hash(&mut hasher);
     hasher.finish()
-}
-
-pub(crate) fn build_hashmap() -> HashMap<u64, usize, MirrorHashBuilder> {
-    HashMap::with_capacity_and_hasher(1024, MirrorHashBuilder::default())
 }
 
 #[derive(Default)]
